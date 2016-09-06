99d73adbf07640a033279505f2f3caa24bb2f296
Alessandro Lepore <lepore.ale@gmail.com>
2016-09-06 18:34:47 +0200

    fix route name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "cookies-policy", to: "eu_cookies#policy", constraints: ->(_) {
+  get "cookies-policy", to: "eu_cookies#policy",
+                        as: :cookies_policy,
+                        constraints: ->(_) {
                           EuCookies.enabled && EuCookies.display_policy == true
                         }
 end
